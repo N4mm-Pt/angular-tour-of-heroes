c3837e36966d6aa63c71e931d7dcb7794c02dc01
hiendt <hiendt123@gmail.com>
2023-02-10 16:01:26 +0700

    10/02

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +9,7 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   hero: Hero = {
     id: 1,
     name : 'Windstorm'
@@ -16,5 +17,12 @@ export class HeroesComponent {
   selectedHero?: Hero
   onSelect(hero : Hero): void{
     this.selectedHero = hero;
+  }
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void{
+    this.getHeroes();
   }
 }
