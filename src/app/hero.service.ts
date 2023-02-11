3b51ac9bf41afd03189a8e9c33e8216585484904
hiendt <hiendt123@gmail.com>
2023-02-11 10:05:59 +0700

    11/02

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable,of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
-  getHeroes() : Hero[]{
-    return HEROES;
+  constructor(private messageService: MessageService) { }
+  getHeroes():Observable<Hero[]>{
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+
+  selectedHero?: Hero;
+
   heroes: Hero[] = [];
-  hero: Hero = {
-    id: 1,
-    name : 'Windstorm'
-  }
-  selectedHero?: Hero
-  onSelect(hero : Hero): void{
-    this.selectedHero = hero;
-  }
-  constructor(private heroService: HeroService) {}
-  getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
-  }
-  ngOnInit(): void{
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
     this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
